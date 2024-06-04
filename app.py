1e3c25c661ebca709c6389c969aca926c24ac3e7
FerUlaje <ulajefernando@gmail.com>
2024-06-04 14:47:22 -0600

    Script2

--- app.py
+++ app.py
@@ -7,11 +7,33 @@ hist_button = st.button('Construir histograma') # crear un botón
 
 if hist_button:
     # escribir un mensaje
-    st.write('Creacióon de un histograma para el conjunto de datos de anuncios de ventas de coches')
+    st.write('Creación de un histograma para el conjunto de datos de anuncios de ventas de coches')
 
     # crear un histograma
     fig = px.histogram(car_data, x='odometer')
 
     # mostrar un gráfico Plotly interactivo
     st.plotly_chart(fig, use_container_width=True)
-    
+
+disp_button = st.button('Construir gráfico de dispersión')
+
+if disp_button:
+    # escribir un mensaje
+    st.write('Creación de un gráfico de dispersión para el conjunto de datos de anuncios de ventas de coches')
+
+    # crear un gráfico de dispersión
+    fig2 = px.scatter(car_data, x='odometer', y='price')
+
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig2, use_container_width=True)
+
+# crear una casilla de verificación
+build_histogram = st.checkbox('Construir un histograma')
+
+if build_histogram:
+    st.write('Construir un histograma para la columna edómetro')
+    # crear un histograma
+    fig = px.histogram(car_data, x='odometer')
+
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
